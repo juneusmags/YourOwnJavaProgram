b6507832a3ccad1c611b085e98c8e052acbf56ff
Juneus Magsakay <jbm9397@gmail.com>
2021-06-07 14:13:47 -0400

    initial commit

---
+++ YourOwnName.java
@@ -0,0 +1,8 @@
+public class YourOwnName{
+    public static void main(String[] args){
+        System.out.println("My name is Juneus Magsakay");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is D.C");
+
+    }
+}
